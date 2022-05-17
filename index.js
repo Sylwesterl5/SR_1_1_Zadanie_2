f80b907a0cea694120283ef703dd110d3344548d
Sylwesterl5 <68221473+Sylwesterl5@users.noreply.github.com>
2022-05-17 12:40:13 +0200

    finish

---
+++ index.js
@@ -0,0 +1,16 @@
+let getDigit = parseInt(prompt('wprowadź cyfrę od 0 do 10'));
+let random = Math.floor(Math.random() * 11);
+
+if(Number.isInteger(getDigit)){
+    if((0<getDigit) && (getDigit<11)){
+        if(random === getDigit){
+            console.log("udało się");
+        } else {
+            console.log("Spróbuj ponownie");
+        }
+    } else{
+        console.log("liczba nie znajduje się w zakresie od 1 do 10");
+    }
+}else {
+    console.log("podany znak nie jest liczbą");
+}
