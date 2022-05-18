41a8d1c9a9fa191fecc8833b380c7f1838fb0190
Sylwesterl5 <68221473+Sylwesterl5@users.noreply.github.com>
2022-05-18 22:30:58 +0200

    fix the code

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-let getDigit = parseInt(prompt('wprowadź cyfrę od 0 do 10'));
-let random = Math.floor(Math.random() * 11);
+const getDigit = parseInt(prompt('wprowadź cyfrę od 0 do 10'));
+const random = Math.floor(Math.random() * 11);
 
-if(Number.isInteger(getDigit)){
-    if((0<getDigit) && (getDigit<11)){
-        if(random === getDigit){
+if (Number.isInteger(getDigit)) {
+    if ((0<getDigit) && (getDigit<11)) {
+        if (random === getDigit) {
             console.log("udało się");
         } else {
             console.log("Spróbuj ponownie");
         }
-    } else{
+    } else {
         console.log("liczba nie znajduje się w zakresie od 1 do 10");
     }
-}else {
+} else {
     console.log("podany znak nie jest liczbą");
-    }
+}
 
